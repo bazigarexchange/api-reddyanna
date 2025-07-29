8c935208b76714f9d93ecc32a385291616ab5632
Prince Verma <bazigarexchangeofficial1@gmail.com>
2025-07-29 20:12:44 +0530

    Google Tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from 'next/script'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,21 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+
+    {/* Google Ads (Global Site Tag - gtag.js) */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=AW-17394843260"
+        strategy="afterInteractive"
+      />
+      <Script id="google-ads" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'AW-17394843260');
+        `}
+      </Script>
+
         {children}
       </body>
     </html>
